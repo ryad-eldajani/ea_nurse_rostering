9cc0ef68d05e72cae4306f722e1c7059ad71b369
nicolasmaeke <nicolas-maeke@gmx.de>
2017-05-14 21:02:02 +0200

    MatingSelectionOperator-Class added (StochasticUniversalSampling). Furthermore a mistake with iterating the population in an other operator was fixed.

--- src/model/ea/operators/StochasticUniversalSampling.java
+++ src/model/ea/operators/StochasticUniversalSampling.java
@@ -15,15 +15,12 @@ import model.ea.Population;
  * @author nicolasmaeke
  *
  */
-public class FitnessProportionalSelection implements IMatingSelectionOperator {
+public class StochasticUniversalSampling implements IMatingSelectionOperator {
 	private int numberOfParents = ConfigurationHelper.getInstance().getPropertyInteger("NumberOfParents", 6);
 	
-	/**
-	 * @param population: the current population
-	 * @return List of individuals for recombination (Parents)
-	 */
 	@Override
 	public List<Individual> select(Population population) {
+		
 		int populationSize = population.getPool().size();
 		float[] cumulatedFitness = new float[populationSize];
 		cumulatedFitness[0] = population.getPool().get(0).getFitness(); 
@@ -32,14 +29,15 @@ public class FitnessProportionalSelection implements IMatingSelectionOperator {
 		for (int i = 1; i < populationSize ; i++) {
 			cumulatedFitness[i] = cumulatedFitness[i-1] + population.getPool().get(i).getFitness(); 
 		}
+		int u = new Random().nextInt((int) (cumulatedFitness[populationSize-1] + 1)/numberOfParents);
+		int j = 0;
 		List<Individual> selection = new ArrayList<Individual>();
 		for (int i = 0; i < numberOfParents; i++) {
-			int j = 0;
-			int u = new Random().nextInt((int) cumulatedFitness[populationSize-1] + 1);
 			while (cumulatedFitness[j] < u){
 				j ++;
-			selection.add(population.getPool().get(j));
 			}
+			u = (int) (u + (cumulatedFitness[populationSize-1]/numberOfParents));
+			selection.add(population.getPool().get(j));
 		}
 		return selection;
 	}
