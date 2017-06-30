a6f17d7b97aa226c882c1aa326ac58dfaeb24684
nicolasmaeke <nicolas-maeke@gmx.de>
2017-06-30 14:18:09 +0200

    Nothing Done

--- src/helper/ClassLoaderHelper.java
+++ src/helper/ClassLoaderHelper.java
@@ -1,11 +1,11 @@
 package helper;
 
 import model.ea.constraints.IFitnessCalculator;
-import model.ea.operators.IEnvironmentSelectionOperator;
-import model.ea.operators.IMutationOperator;
-import model.ea.operators.IRecombinationOperator;
+import model.ea.operators.IEnvironmentSelection;
+import model.ea.operators.IMutation;
+import model.ea.operators.IRecombination;
 import model.ea.construction.IConstructionHeuristic;
-import model.ea.operators.IMatingSelectionOperator;
+import model.ea.operators.IMatingSelection;
 import parser.IParser;
 import writer.IWriter;
 
@@ -94,52 +94,52 @@ public class ClassLoaderHelper {
      * Returns the implementing mating selection operator.
      * @return Instance that implements IConstructionHeuristic
      */
-    public IMatingSelectionOperator getMatingSelectionOperator() {
+    public IMatingSelection getMatingSelectionOperator() {
         Object instance = getLoadedClass("MatingSelectionOperator", "SimpleMatingSelectionOperator", "model.ea.operators.");
         if (instance == null) {
             return null;
         }
 
-        return (IMatingSelectionOperator) instance;
+        return (IMatingSelection) instance;
     }
 
     /**
      * Returns the implementing recombination operator.
      * @return Instance that implements IRecombinationOperator
      */
-    public IRecombinationOperator getRecombinationOperator() {
+    public IRecombination getRecombinationOperator() {
         Object instance = getLoadedClass("RecombinationOperator", "SimpleRecombinationOperator", "model.ea.operators.");
         if (instance == null) {
             return null;
         }
 
-        return (IRecombinationOperator) instance;
+        return (IRecombination) instance;
     }
 
     /**
      * Returns the implementing mutation operator.
      * @return Instance that implements IMutationOperator
      */
-    public IMutationOperator getMutationOperator() {
+    public IMutation getMutationOperator() {
         Object instance = getLoadedClass("MutationOperator", "SimpleMutationOperator", "model.ea.operators.");
         if (instance == null) {
             return null;
         }
 
-        return (IMutationOperator) instance;
+        return (IMutation) instance;
     }
 
     /**
      * Returns the implementing environmental selection operator.
      * @return Instance that implements IEnvironmentSelectionOperator
      */
-    public IEnvironmentSelectionOperator getEnvironmentSelectionOperator() {
+    public IEnvironmentSelection getEnvironmentSelectionOperator() {
         Object instance = getLoadedClass("EnvironmentSelectionOperator", "SimpleEnvironmentSelectionOperator", "model.ea.operators.");
         if (instance == null) {
             return null;
         }
 
-        return (IEnvironmentSelectionOperator) instance;
+        return (IEnvironmentSelection) instance;
     }
 
     /**
